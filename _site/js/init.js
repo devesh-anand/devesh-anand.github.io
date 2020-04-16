9984c48a8856189378d61a8597319efb8f9311e1
devesh-anand <contactdeveshanand@gmail.com>
2020-04-16 22:09:02 +0530

    done

--- _site/js/init.js
+++ _site/js/init.js
@@ -8,12 +8,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global: { href: '/Read-Only-Jekyll-Theme/css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
-			xlarge: { media: '(max-width: 1680px)', href: '/Read-Only-Jekyll-Theme/css/style-xlarge.css' },
-			large: { media: '(max-width: 1280px)', href: '/Read-Only-Jekyll-Theme/css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			medium: { media: '(max-width: 1024px)', href: '/Read-Only-Jekyll-Theme/css/style-medium.css', containers: '85%!' },
-			small: { media: '(max-width: 736px)', href: '/Read-Only-Jekyll-Theme/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
-			xsmall: { media: '(max-width: 480px)', href: '/Read-Only-Jekyll-Theme/css/style-xsmall.css' }
+			global: { href: '/css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
+			xlarge: { media: '(max-width: 1680px)', href: '/css/style-xlarge.css' },
+			large: { media: '(max-width: 1280px)', href: '/css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			medium: { media: '(max-width: 1024px)', href: '/css/style-medium.css', containers: '85%!' },
+			small: { media: '(max-width: 736px)', href: '/css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
+			xsmall: { media: '(max-width: 480px)', href: '/css/style-xsmall.css' }
 		},
 		plugins: {
 			layers: {
