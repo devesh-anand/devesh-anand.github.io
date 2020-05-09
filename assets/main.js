a4dced5fd95da53d41fed2d9edf9b1a296bbb480
devesh-anand <contactdeveshanand@gmail.com>
2020-05-09 11:52:28 +0530

    comments fixed

--- assets/main.js
+++ assets/main.js
@@ -18,7 +18,7 @@
         data: $(this).serialize(),
         contentType: 'application/x-www-form-urlencoded',
         success: function (data) {
-          showModal('Comment submitted', 'Thanks! Your comment is <a href="https://github.com/travisdowns/travisdowns.github.io/pulls">pending</a>. It will appear when approved.');
+          showModal('Comment submitted', 'Thanks! Your comment is pending. It will appear when approved.');
   
           $("#comment-form-submit")
             .html("Submit");
